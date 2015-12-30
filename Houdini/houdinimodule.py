455bd8d6aaa57fb9cc9f06e10a4a0b10a4afe7e2
forgacs.daniel <forgacs.daniel@gmail.com>
2015-12-30 10:39:16 +0000

    started houdini modules

---
+++ Houdini/houdinimodule.py
@@ -0,0 +1,21 @@
+"""
+shelf tool:
+    import sys
+
+    modulepath = 'c:/_STORAGE/dev/vfx-unittest/Houdini'
+
+    if modulepath not in sys.path:
+        sys.path.append(modulepath)
+
+    import houdinimodule as hm
+    reload(hm)
+
+    hm.main()
+"""
+
+
+# import hou
+
+
+def main():
+    print('OKasdcf')
---
+++ Houdini/tests_houdinimodule.py
@@ -0,0 +1,18 @@
+"""
+shelf tool:
+    import sys
+
+    modulepath = 'c:/_STORAGE/dev/vfx-unittest/Houdini'
+
+    if modulepath not in sys.path:
+        sys.path.append(modulepath)
+
+    import tests_houdinimodule as thm
+    reload(thm)
+
+    thm.main()
+"""
+
+
+def main():
+    print('tesasdasdt')
